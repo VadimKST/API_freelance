3e8aa7ab419a8b219f0a0ba854dbc6e0c4b05272
VadimKST <vadstge@gmail.com>
2021-04-27 13:02:05 +0300

    Settings modified

---
+++ freelance_api/secret_settings.py
@@ -0,0 +1,2 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-nl^wsu9@9_7v0am=r+7zep_l+&8waez(isfi5=cly0qgfo01+!'
--- freelance_api/settings.py
+++ freelance_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from .secret_settings import *
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -18,9 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nl^wsu9@9_7v0am=r+7zep_l+&8waez(isfi5=cly0qgfo01+!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
